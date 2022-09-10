215d4cba6890f3dd6ce4a1fb52dd8ffa36b03283
Precious Ighereno <=>
2022-09-10 06:25:29 +0100

    completed v1

--- MyResume/MyResume/settings.py
+++ MyResume/MyResume/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'MyResume.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    'C:/Users/HP/Desktop/Projects/resume/MyResume/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
